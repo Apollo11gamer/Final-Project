a66db498fd2a30aa79fc85122be250fc075cdad6
Apollo11gamer <Jriley21708@gmail.com>
2025-01-29 10:12:08 -0500

    Jay 1/29/25

---
+++ Password.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Password {
+
+int password;
+String Astronaut;
+
+Scanner kbd = new Scanner(System.in);
+    public static void main(String[] args) {
+
+
+
+
+
+}
+}

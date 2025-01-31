f9947f6c36448aa15de815c41b84cba7e5fa3905
Alyssa <000260219@student.baisd.net>
2025-01-31 09:07:50 -0500

    Alyssa 01/31/2025
    added fuel and flight mechanics (wip)

--- LaunchSequence.java
+++ LaunchSequence.java
@@ -1,57 +1,76 @@
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.Scanner;
 
 public class LaunchSequence {
 
     public static void main(String[] args) {
 
+        // Flight and fuel
+        double altitude = 0;
+        double fuelBurned = 0;
+        double speed;
+        double currentFuel;
+        Scanner kbd = new Scanner(System.in);
 
-    // 10 Second Countdown
-    int time = 10;
-    double fuelBurned;
-    double altitude;
-    Timer timer = new Timer();
+        System.out.println("Enter currentFuel: ");
+        currentFuel = kbd.nextDouble();
 
-    timer.scheduleAtFixedRate(new TimerTask() {
-    int countDown = time;
+        System.out.println("Enter fuelBurned: ");
+        fuelBurned = kbd.nextDouble();
 
-    public void run() {
-    System.out.println(countDown);
+        currentFuel =- fuelBurned;
+        speed = fuelBurned * 30;
+        altitude = Math.pow(speed, 2);
+        System.out.println(altitude);
 
-    if (countDown <= 0) {
-    System.out.println("LAUNCH");
-    timer.cancel();
+        // 10 Second Countdown
+        kbd.nextLine();
+        int time = 10;
+        Timer timer = new Timer();
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+        int countDown = time;
+
+        public void run() {
+        System.out.println(countDown);
+        
+
+        if (countDown <= 0) {
+        System.out.println("LAUNCH");
+        timer.cancel();
+        }
+        countDown--;
+        }
+        }, 0, 1000);
+        
+
+        if (altitude >= 70000) {
+
+        // 30 Second Timer
+        int spacewalkTime = 30;
+        Timer spacewalkTimer = new Timer();
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+        int spacewalkCountdown = spacewalkTime;
+
+        public void run() {
+        System.out.println(spacewalkCountdown);
+
+        if (spacewalkCountdown <= 0) {
+        System.out.println("SPACEWALKED");
+        spacewalkTimer.cancel();
+        }
+        spacewalkCountdown--;
+        }
+        }, 0, 1000);
+
+        }
+        else if (altitude < 70000) {
+        System.out.println("Spacecraft did not make it.");
+        }
+
     }
-    countDown--;
-    }
-    }, 0, 1000);
-    
-
-    // 30 Second Timer
-    int spacewalkTime = 30;
-    Timer spacewalkTimer = new Timer();
-
-    timer.scheduleAtFixedRate(new TimerTask() {
-    int spacewalkCountdown = spacewalkTime;
-
-    public void run() {
-    System.out.println(spacewalkCountdown);
-
-    if (spacewalkCountdown <= 0) {
-    System.out.println("SPACEWALKED");
-    spacewalkTimer.cancel();
-    }
-    spacewalkCountdown--;
-    }
-    }, 0, 1000);
-
-    }
-
-
-    // Flight and fuel
-    double altitude;
-    double fuelBurned;
-
     
     }
 

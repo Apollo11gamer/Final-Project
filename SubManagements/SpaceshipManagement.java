1ac1d53132da5abd6a25d932b51ecac4203904e9
Apollo11gamer <Jriley21708@gmail.com>
2025-02-11 10:14:53 -0500

    JR

--- SubManagements/SpaceshipManagement.java
+++ SubManagements/SpaceshipManagement.java
@@ -2,38 +2,44 @@ package SubManagements;
 
 import java.util.Scanner;
 
-public class SpaceshipManagement{
+public class SpaceshipManagement {
 
-public static void Ship() {
+    public static void Ship() {
+        String shuttleName;
+        double fuelCapacity;
+        int astronautCapacity;
+        Scanner kbd = new Scanner(System.in);
 
-    String shuttleName;
-    double fuelCapacity;
-    int astronautCapacity;
-    Scanner kbd = new Scanner(System.in);
-
-
-    System.out.println("What is the name of the shuttle?");
+        System.out.println("What is the name of the shuttle?");
         shuttleName = kbd.nextLine();
 
-
-    System.out.println("What is the maximum amount of fuel(pounds of fuel) for the spaceship?");
+        System.out.println("What is the maximum amount of fuel(pounds of fuel) for the spaceship?");
         fuelCapacity = kbd.nextDouble();
 
-
-    System.out.println("What is the maximum amount of astronauts that can be a part of the crew?");
+        System.out.println("What is the maximum amount of astronauts that can be a part of the crew?");
         astronautCapacity = kbd.nextInt();
 
         kbd.close();
 
         if (fuelCapacity > 10000) {
-            System.out.println("The ship, " + "'" + shuttleName + "'" + ", will be attempting to launch to the moon. There are " + astronautCapacity + " member(s) with " + fuelCapacity + " pounds of fuel to work with." );
-            System.exit(0);
-    }
-        else {
+            System.out.println("The ship, '" + shuttleName + "', will be attempting to launch to the moon. There are " + astronautCapacity + " member(s) with " + fuelCapacity + " pounds of fuel to work with.");
+            proceedToNextFile();
+        } else {
             System.out.println(shuttleName + " requires more fuel to leave the planet, please try again.");
-        System.exit(0);
+        }
     }
 
-}
+    // Moved outside of Ship() method
+    private static void proceedToNextFile() {
+        try {
+            System.out.println("Proceeding to Launch sequence...");
+            Launch.launch();  // Make sure EmailVerifier.Email() exists
+        } catch (Exception e) {
+            System.out.println("Error starting Lauch sequence: " + e.getMessage());
+        }
+    }
 
+    public static void ship() {
+        Ship();  // Calling the Ship method in main
+    }
 }

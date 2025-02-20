4d4abba859e2af25ef8265182bf86241bb03c4f7
Alyssa <000260219@student.baisd.net>
2025-02-20 08:02:05 -0500

    Alyssa 02/20/2025
    Added some documentation to EntryPoint.java.

--- EntryPoint.java
+++ EntryPoint.java
@@ -11,19 +11,23 @@ public class EntryPoint{
 
 public static void main(String[] args) {
 
+    // Prints out a welcome message for the user.
     System.out.println("===============================================");
     System.out.println(" | Welcome to BAISD Astronaut Control Panel |");
     System.out.println("===============================================");
 
+    // Provides the user with a menu to log in to make astronauts and spaceships, and to exercise admin controls.
     Password.pass();
+    // Verifies if the email is written correctly.
     EmailVerifier.Email();
+    // Verifies if the date of birth of the astronaut is correctly written.
     DOBDetector.DOB();
+    // Lets the user create an astronaut and their details.
     AstronautManagement.Space();
+    // Lets the user create a spaceship and their details.
     SpaceshipManagement.Ship();
+    // Launches the user's spaceship into outer space, has the astronauts proceed with a spacewalk, then returns back to Earth.
     Launch.launch();
-
-
-
 
 System.exit(0);
 }
